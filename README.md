a0bec10002bb25f0857ae1798882c8761e5acf15
cqroot <cqroot@outlook.com>
2021-10-15 13:44:59 +0800

    feat: add total disk capacity data

--- README.md
+++ README.md
@@ -10,12 +10,15 @@ OpenStack Swift Exporter for Prometheus.
 |:------------------------------|:-------------------------------------|
 | swift_account_server_status   | Swift account-server reachability.   |
 | swift_container_server_status | Swift container-server reachability. |
-| swift_object_server_status      | Swift object-server reachability.    |
+| swift_object_server_status    | Swift object-server reachability.    |
 
 ### Disk
 
-| Name                   | Description |
-|:-----------------------|:------------|
-| swift_disk_avail_bytes |             |
-| swift_disk_used_bytes  |             |
-| swift_disk_size_bytes  |             |
+| Name                         | Description |
+|:-----------------------------|:------------|
+| swift_disk_avail_bytes       |             |
+| swift_disk_used_bytes        |             |
+| swift_disk_size_bytes        |             |
+| swift_disk_total_avail_bytes |             |
+| swift_disk_total_used_bytes  |             |
+| swift_disk_total_size_bytes  |             |
--- internal/disk.go
+++ internal/disk.go
@@ -10,10 +10,13 @@ import (
 )
 
 type diskCollector struct {
-	logger         *logrus.Logger
-	usedBytesDesc  *prometheus.Desc
-	availBytesDesc *prometheus.Desc
-	sizeBytesDesc  *prometheus.Desc
+	logger              *logrus.Logger
+	usedBytesDesc       *prometheus.Desc
+	availBytesDesc      *prometheus.Desc
+	sizeBytesDesc       *prometheus.Desc
+	totalUsedBytesDesc  *prometheus.Desc
+	totalAvailBytesDesc *prometheus.Desc
+	totalSizeBytesDesc  *prometheus.Desc
 }
 
 func init() {
@@ -36,6 +39,18 @@ func NewDiskCollector(logger *logrus.Logger) Collector {
 			prometheus.BuildFQName(namespace, "disk", "size_bytes"),
 			"Swift disk size.", []string{"host", "device"}, nil,
 		),
+		totalUsedBytesDesc: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "disk", "total_used_bytes"),
+			"Swift disk total used.", nil, nil,
+		),
+		totalAvailBytesDesc: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "disk", "total_avail_bytes"),
+			"Swift disk total avail.", nil, nil,
+		),
+		totalSizeBytesDesc: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "disk", "total_size_bytes"),
+			"Swift disk total size.", nil, nil,
+		),
 	}
 }
 
@@ -43,8 +58,15 @@ func (collector *diskCollector) Update(ch chan<- prometheus.Metric) error {
 	wg := sync.WaitGroup{}
 	wg.Add(len(swiftInfo.Object))
 
+	wgTotal := sync.WaitGroup{}
+	wgTotal.Add(3)
+
+	chUsed := make(chan float64)
+	chAvail := make(chan float64)
+	chSize := make(chan float64)
+
 	for _, objectInfo := range swiftInfo.Object {
-		go func(wg *sync.WaitGroup, ch chan<- prometheus.Metric, host string, port string, devices []string) {
+		go func(wg *sync.WaitGroup, ch chan<- prometheus.Metric, host string, port string, devices []string, chUsed chan<- float64, chAvail chan<- float64, chSize chan<- float64) {
 			defer wg.Done()
 
 			diskUsage, err := getDiskUsage(host, port)
@@ -70,11 +92,55 @@ func (collector *diskCollector) Update(ch chan<- prometheus.Metric) error {
 				ch <- prometheus.MustNewConstMetric(collector.usedBytesDesc, prometheus.GaugeValue, disk["used"].(float64), host, disk["device"].(string))
 				ch <- prometheus.MustNewConstMetric(collector.availBytesDesc, prometheus.GaugeValue, disk["avail"].(float64), host, disk["device"].(string))
 				ch <- prometheus.MustNewConstMetric(collector.sizeBytesDesc, prometheus.GaugeValue, disk["size"].(float64), host, disk["device"].(string))
+
+				chUsed <- disk["used"].(float64)
+				chAvail <- disk["avail"].(float64)
+				chSize <- disk["size"].(float64)
 			}
-		}(&wg, ch, objectInfo.Host, objectInfo.Port, objectInfo.Devices)
+		}(&wg, ch, objectInfo.Host, objectInfo.Port, objectInfo.Devices, chUsed, chAvail, chSize)
 	}
 
+	go func(wg *sync.WaitGroup, ch chan<- prometheus.Metric, chUsed <-chan float64) {
+		defer wg.Done()
+
+		var totalUsed float64
+
+		for used := range chUsed {
+			totalUsed += used
+		}
+		ch <- prometheus.MustNewConstMetric(collector.totalUsedBytesDesc, prometheus.GaugeValue, totalUsed)
+	}(&wgTotal, ch, chUsed)
+
+	go func(wg *sync.WaitGroup, ch chan<- prometheus.Metric, chAvail <-chan float64) {
+		defer wg.Done()
+
+		var totalAvail float64
+
+		for avail := range chAvail {
+			totalAvail += avail
+		}
+		ch <- prometheus.MustNewConstMetric(collector.totalAvailBytesDesc, prometheus.GaugeValue, totalAvail)
+	}(&wgTotal, ch, chAvail)
+
+	go func(wg *sync.WaitGroup, ch chan<- prometheus.Metric, chSize <-chan float64) {
+		defer wg.Done()
+
+		var totalSize float64
+
+		for size := range chSize {
+			totalSize += size
+		}
+		ch <- prometheus.MustNewConstMetric(collector.totalSizeBytesDesc, prometheus.GaugeValue, totalSize)
+	}(&wgTotal, ch, chSize)
+
 	wg.Wait()
+
+	close(chUsed)
+	close(chAvail)
+	close(chSize)
+
+	wgTotal.Wait()
+
 	return nil
 }
 
